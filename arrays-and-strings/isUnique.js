9a572e792b618d5db4af6818559870569c2281ce
Winnie Chou <winnie.chou.mb@gmail.com>
2021-04-07 17:35:51 -0500

    oops. this is updating isUnique and removing old permutation solution

--- arrays-and-strings/isUnique.js
+++ arrays-and-strings/isUnique.js
@@ -3,15 +3,21 @@
 // Test cases
 // 'abcde' => true
 // 'aaa' => false
+// 'aab' => false
+// 'aba' => false
+// '' => false
 
 /** Function determines if a string has all unique characters
  *  Returns true or false
+ * 
+ *  Assumption that the string is in ASCII, max 128 characters
  * 
  *  Time complexity: O(n) where n is the length of the string
  *  Space complexity: O(1)
  */
 function isUnique(str){
   if (str.length === 0) return false;
+  if (str.length > 128) return false;
 
   let charSeen = new Map(); // Create a hash to store letters seen
 
